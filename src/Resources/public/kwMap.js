09b25a6d2e2e10d34f58e006a83b9ffd84e39f60
Klaus <support@kikwik.it>
2023-10-06 09:53:05 +0200

    opacity = 1 in SingleColorRenderer for mapCluster

--- src/Resources/public/kwMap.js
+++ src/Resources/public/kwMap.js
@@ -295,7 +295,7 @@ class SingleColorRenderer {
     render({ count, position }, stats, map) {
         // create svg url with fill color
         const svg = `<svg fill="${this.color}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 240">
-                  <circle cx="120" cy="120" opacity=".6" r="70" />
+                  <circle cx="120" cy="120" opacity="1" r="70" />
                   <circle cx="120" cy="120" opacity=".3" r="90" />
                   <circle cx="120" cy="120" opacity=".2" r="110" />
                 </svg>`;
